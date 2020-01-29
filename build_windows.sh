1ecf0b2b471424e0a2910b8c27f1f183ecfb023b
Liam O'Connor <me@liamoc.net>
2020-01-30 04:10:29 +1100

    windows now builds in icons

--- build_windows.sh
+++ build_windows.sh
@@ -1,7 +1,8 @@
 rm -rf tmp
-mkdir tmp
+mkdir -p tmp
 cd $1 && cargo build --release && cd ..
 cp target/release/$1.exe tmp/
 cp /mingw64/bin/libSDL2_* tmp/
 cp /mingw64/bin/SDL2* tmp/
+cp $1/icon_windows.ico tmp/ && cd tmp && "C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open $1.exe -save $1.exe -action add -resource icon_windows.ico -mask ICONGROUP,MAINICON,0 && rm icon_windows.ico && cd ..
 butler push tmp liamoc/$2:windows
